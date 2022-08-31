5207a27deceafd043a39296cf6568d0a2b13f0e6
suresh <sureshpandu114@gmail.com>
2022-08-31 20:21:11 +0530

    ecommerce ui design

---
+++ src/components/Footer.jsx
@@ -0,0 +1,132 @@
+import React from 'react'
+import styled from 'styled-components';
+import {Pinterest, Facebook, Instagram, Twitter, Room, Phone,MailOutline} from '@material-ui/icons'
+import { mobile } from "../pages/responsive";
+
+const Container = styled.div`
+  display: flex;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ display: "none" })}
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Listitem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+`;
+const Footer = () => {
+  return (
+    <Container>
+        <Left>
+            <Logo>SURESH.</Logo>
+            <Desc> There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don’t look even slightly believable.</Desc>
+          <SocialContainer>
+            <SocialIcon color='3B5999'>
+                <Facebook />
+            </SocialIcon>
+            <SocialIcon color='E4405F'>
+                <Instagram />
+            </SocialIcon>
+            <SocialIcon color='55ACEE'>
+                <Twitter />
+            </SocialIcon>
+            <SocialIcon color='E60023'>
+                <Pinterest />
+            </SocialIcon>
+          </SocialContainer>
+        </Left>
+        <Center>
+            <Title>Useful Links</Title>
+            <List>
+                <Listitem>Home</Listitem>
+                <Listitem>Cart</Listitem>
+                <Listitem>Man Fashion</Listitem>
+                <Listitem>Woman Fashion</Listitem>
+                <Listitem>My Account</Listitem>
+                <Listitem>Order Tracking</Listitem>
+                <Listitem>Wishlist</Listitem>
+                <Listitem>Terms</Listitem>
+            </List>
+        </Center>
+        <Right>
+            <Title>Contact</Title>
+            <ContactItem>
+               <Room style={{marginRight:"10px"}}/> Bangalore, Kundanahalli gate 
+            </ContactItem>
+            <ContactItem>
+                <Phone style={{marginRight:"10px"}}/>+91 8309575066
+            </ContactItem>
+            <ContactItem>
+                <MailOutline style={{marginRight:"10px"}}/>sureshpandu114@gmail.com
+            </ContactItem>
+            <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+        </Right>
+        
+    </Container>
+  )
+}
+
+export default Footer
